1e69878e5a72b7f87089219d266b15c526d13d38
Maldonado <joseromaldo.1997@gmail.com>
2024-09-16 21:41:02 -0600

    Creacion de vista para email

---
+++ views/email/saludo.php
@@ -0,0 +1,19 @@
+<style>
+    h1 {
+    color: green;
+    font-size: 36px;
+    font-family: Arial, sans-serif;
+    text-align: center;
+    font-weight: bold; 
+    text-transform: uppercase; 
+    letter-spacing: 2px; 
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); /* Aplica una sombra al texto */
+    margin-top: 20px; 
+    padding-bottom: 10px;
+    border-bottom: 2px solid green; 
+}
+
+</style>
+
+<h1>A/F JOSE RODRIGO MALDONADO MORALES</h1>
+<img src="cid:hola" alt="Logo Institucional">
---
+++ views/pdf/reporte.php
@@ -0,0 +1,4 @@
+<h1>Alférez de Fragata JOSE RODRIGO MALDONADO MORALES</h1>
+<p>Esta es una prueba de envío de PDF por EMAIL</p>
+
+<pagebreak resetpagenum="1" pagenumstyle="a" suppress="off" />
